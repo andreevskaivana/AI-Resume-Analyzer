05e9ec605acfb059e0c85705cd4a57bf0260394e
andreevskaivana <ivana.andreevska25@gmail.com>
2025-11-12 01:02:25 +0100

    Home Page , Resume Card and initial design and types

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">
+                    Resumite
+                </p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+            </Link>
+        </nav>
+    );
+};
+
